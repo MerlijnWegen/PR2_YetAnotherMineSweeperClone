f6203fa20dfc1d5ca489ded9b9a2d43757b02a9d
Merlijn Wegen <merlijn.wegen@gmail.com>
2026-01-07 16:16:40 +0100

    comment in bat for my own sanity

--- MineSweeper.bat
+++ MineSweeper.bat
@@ -1,3 +1,4 @@
 @echo off
 
+:: get filepath from cmd, convert to wsl filepath, use bash to run the command "cd \filepath\" then run "./minesweeper"
 wsl bash -lc "cd \"$(wslpath '%~dp0')\" && ./minesweeper"
